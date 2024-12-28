f674749f90bec3963e3e16a9a6118a41996a8a5e
koyo922 <koyo922@qq.com>
2024-12-29 01:53:58 +0800

    mock priceFeed for anvil

--- updraft_course/foundry_fund_me/script/HelperConfig.s.sol
+++ updraft_course/foundry_fund_me/script/HelperConfig.s.sol
@@ -3,12 +3,14 @@
 pragma solidity ^0.8.19;
 
 import {Script} from "forge-std/Script.sol";
+import {MockV3Aggregator} from "../test/mocks/MockV3Aggregator.sol";
 
-contract HelperConfig {
+contract HelperConfig is Script {
     struct NetworkConfig {
         address priceFeed; // ETH/USD price feed address
     }
     NetworkConfig public activeNetworkConfig;
+    MockV3Aggregator public mockPriceFeed;
 
     constructor() {
         if (block.chainid == 11155111) {
@@ -18,12 +20,17 @@ contract HelperConfig {
         }
     }
 
-    function getSepoliaEthConfig() public pure returns (NetworkConfig memory) {
+    function getSepoliaEthConfig() public returns (NetworkConfig memory) {
         NetworkConfig memory sepoliaConfig = NetworkConfig({
             priceFeed: 0x694AA1769357215DE4FAC081bf1f309aDC325306
         });
         return sepoliaConfig;
     }
 
-    function getAnvilEthConfig() public pure returns (NetworkConfig memory) {}
+    function getAnvilEthConfig() public returns (NetworkConfig memory) {
+        vm.startBroadcast();
+        mockPriceFeed = new MockV3Aggregator(8, 2000e8);
+        vm.stopBroadcast();
+        return NetworkConfig({priceFeed: address(mockPriceFeed)});
+    }
 }
