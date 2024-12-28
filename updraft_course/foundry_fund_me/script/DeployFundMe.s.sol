9f090742e3f61ed53d1809cfa2f95866568968ff
koyo922 <koyo922@qq.com>
2024-12-28 22:50:07 +0800

    deploy using sepolia-eth address of priceFeed.

--- updraft_course/foundry_fund_me/script/DeployFundMe.s.sol
+++ updraft_course/foundry_fund_me/script/DeployFundMe.s.sol
@@ -5,9 +5,11 @@ import {FundMe} from "../src/FundMe.sol";
 import {Script} from "forge-std/Script.sol";
 
 contract DeployFundMe is Script {
-    function run() external {
+    function run() external returns (FundMe) {
         vm.startBroadcast();
-        new FundMe(address(0));
+        // Sepolia ETH/USD Price Feed 地址
+        FundMe fundMe = new FundMe(0x694AA1769357215DE4FAC081bf1f309aDC325306);
         vm.stopBroadcast();
+        return fundMe;
     }
 }
