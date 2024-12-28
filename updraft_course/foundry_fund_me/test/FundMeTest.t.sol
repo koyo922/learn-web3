96a15c7dd9d49c99dced45b7ada5152e53df6e31
koyo922 <koyo922@qq.com>
2024-12-29 03:11:21 +0800

    better coverage and modifier

--- updraft_course/foundry_fund_me/test/FundMeTest.t.sol
+++ updraft_course/foundry_fund_me/test/FundMeTest.t.sol
@@ -22,6 +22,13 @@ contract FundMeTest is Test {
         vm.deal(alice, STARTING_BALANCE);
     }
 
+    modifier funded() {
+        vm.prank(alice);
+        fundMe.fund{value: SEND_VALUE}();
+        assert(address(fundMe).balance > 0);
+        _;
+    }
+
     // 注：
     // 1. MINIMUM_USD 虽是公共变量，但需要用函数方式访问：fundMe.MINIMUM_USD()
     // 2. 因为此测试仅读取状态，所以加上 view 修饰符
@@ -72,10 +79,68 @@ contract FundMeTest is Test {
         fundMe.fund{value: 0}(); // 显式传入 0 值，预期会 revert
     }
 
-    function testFundUpdatesFundDataStructure() public {
-        vm.prank(alice);
-        fundMe.fund{value: SEND_VALUE}();
+    function testFundUpdatesFundDataStructure() public funded {
         uint256 amountFunded = fundMe.getAddressToAmountFunded(alice);
         assertEq(amountFunded, SEND_VALUE);
+    }
+
+    function testAddsFunderToArrayOfFunders() public funded {
+        address funder = fundMe.getFunder(0);
+        assertEq(funder, alice);
+    }
+
+    function testOnlyOwnerCanWithdraw() public funded {
+        vm.expectRevert();
+        vm.prank(alice);
+        fundMe.withdraw();
+    }
+
+    function testWithdrawFromASingleFunder() public funded {
+        uint256 startingFundMeBalance = address(fundMe).balance;
+        uint256 startingOwnerBalance = fundMe.getOwner().balance;
+
+        vm.startPrank(fundMe.getOwner());
+        fundMe.withdraw();
+        vm.stopPrank();
+
+        uint256 endingFundMeBalance = address(fundMe).balance;
+        uint256 endingOwnerBalance = fundMe.getOwner().balance;
+        assertEq(endingFundMeBalance, 0);
+        assertEq(
+            startingFundMeBalance + startingOwnerBalance,
+            endingOwnerBalance
+        );
+    }
+
+    function testWithdrawFromMultipleFunders() public funded {
+        uint160 numberOfFunders = 10;
+        uint160 startingFunderIndex = 1;
+        for (
+            uint160 i = startingFunderIndex;
+            i < numberOfFunders + startingFunderIndex;
+            i++
+        ) {
+            // we get hoax from stdcheats
+            // prank + deal
+            hoax(address(i), SEND_VALUE);
+            fundMe.fund{value: SEND_VALUE}();
+        }
+
+        uint256 startingFundMeBalance = address(fundMe).balance;
+        uint256 startingOwnerBalance = fundMe.getOwner().balance;
+
+        vm.startPrank(fundMe.getOwner());
+        fundMe.withdraw();
+        vm.stopPrank();
+
+        assert(address(fundMe).balance == 0);
+        assert(
+            startingFundMeBalance + startingOwnerBalance ==
+                fundMe.getOwner().balance
+        );
+        assert(
+            (numberOfFunders + 1) * SEND_VALUE == // +1 是因为 funded modifier 中 alice 已投入一次
+                fundMe.getOwner().balance - startingOwnerBalance
+        );
     }
 }
