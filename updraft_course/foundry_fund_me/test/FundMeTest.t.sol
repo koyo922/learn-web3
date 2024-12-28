e0a0a4c62e3fd6463961e6a3ef275a515a9dd0ef
koyo922 <koyo922@qq.com>
2024-12-28 21:28:35 +0800

    add testMinimumDollarIsFive()

--- updraft_course/foundry_fund_me/test/FundMeTest.t.sol
+++ updraft_course/foundry_fund_me/test/FundMeTest.t.sol
@@ -1,6 +1,22 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
-import {Test} from "../lib/forge-std/src/Test.sol";
+// NOTE: Test contract inherits all assertion functions from forge-std/Test.sol
+// No need to import them separately
+import {Test} from "forge-std/Test.sol";
+import {FundMe} from "../src/FundMe.sol";
 
-contract FundMeTest is Test {}
+contract FundMeTest is Test {
+    FundMe fundMe;
+
+    function setUp() external {
+        fundMe = new FundMe(address(0));
+    }
+
+    // NOTE:
+    // 1. MINIMUM_USD is a public variable, but we need to access it as a function: fundMe.MINIMUM_USD()
+    // 2. Since this test only reads state, we add the view modifier
+    function testMinimumDollarIsFive() public view {
+        assertEq(fundMe.MINIMUM_USD(), 5e18);
+    }
+}
