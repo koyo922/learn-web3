100407908a7b71ec47640c3b239221735a6ecb33
koyo922 <koyo922@qq.com>
2024-12-29 21:04:08 +0800

    define & emit custom Event or revert custom Error

--- updraft_course/foundry_smark_contract_lottery/src/Raffle.sol
+++ updraft_course/foundry_smark_contract_lottery/src/Raffle.sol
@@ -2,6 +2,8 @@
 
 pragma solidity ^0.8.18;
 
+error Raffle_NotEnoughEthSent();
+
 /**
  * @title A sample Raffle Contract
  * @author Patrick Collins (or even better, you own name)
@@ -10,12 +12,21 @@ pragma solidity ^0.8.18;
  */
 contract Raffle {
     uint256 private immutable i_entranceFee;
+    address payable[] private s_players;
+
+    event EnteredRaffle(address indexed player); // 带索引的参数(topics)更易于检索
 
     constructor(uint256 entranceFee) {
         i_entranceFee = entranceFee;
     }
 
-    function enterRaffle() public payable {}
+    // external 比 public 更便宜
+    function enterRaffle() external payable {
+        // require(msg.value >= i_entranceFee, "Not enough ETH sent");
+        if (msg.value < i_entranceFee) revert Raffle_NotEnoughEthSent(); // 自定义Error类型，比require更便宜
+        s_players.push(payable(msg.sender));
+        emit EnteredRaffle(msg.sender);
+    }
 
     function pickWinner() public {}
 
