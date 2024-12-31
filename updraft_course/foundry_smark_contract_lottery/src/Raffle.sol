d9c0d0d6892b968d4953844901cc2f8794ead322
koyo922 <koyo922@qq.com>
2025-01-01 00:34:41 +0800

    add enum RaffleState, use ChainLink Automation to pickWinner

--- updraft_course/foundry_smark_contract_lottery/src/Raffle.sol
+++ updraft_course/foundry_smark_contract_lottery/src/Raffle.sol
@@ -5,8 +5,6 @@ pragma solidity ^0.8.18;
 import {VRFConsumerBaseV2Plus} from "@chainlink/contracts/src/v0.8/vrf/dev/VRFConsumerBaseV2Plus.sol";
 import {VRFV2PlusClient} from "@chainlink/contracts/src/v0.8/vrf/dev/libraries/VRFV2PlusClient.sol";
 
-error Raffle_NotEnoughEthSent();
-
 /**
  * @title A sample Raffle Contract
  * @author Patrick Collins (or even better, you own name)
@@ -14,6 +12,20 @@ error Raffle_NotEnoughEthSent();
  * @dev It implements Chainlink VRFv2.5 and Chainlink Automation
  */
 contract Raffle is VRFConsumerBaseV2Plus {
+    error Raffle_NotEnoughEthSent();
+    error Raffle_TransferFailed();
+    error Raffle_RaffleNotOpen();
+    error Raffle_UpkeepNotNeeded(
+        uint256 balance,
+        uint256 playersLength,
+        uint256 raffleState
+    );
+
+    enum RaffleState {
+        OPEN,
+        CALCULATING
+    }
+
     uint16 private constant REQUEST_CONFIRMATIONS = 3;
     uint32 private constant NUM_WORDS = 1;
     uint256 private immutable i_entranceFee;
@@ -24,8 +36,10 @@ contract Raffle is VRFConsumerBaseV2Plus {
     uint32 private immutable i_callbackGasLimit;
     address payable[] private s_players;
     uint256 private s_lastTimeStamp;
-
+    address payable private s_recentWinner;
+    RaffleState private s_raffleState;
     event EnteredRaffle(address indexed player); // 带索引的参数(topics)更易于检索
+    event WinnerPicked(address indexed winner);
 
     constructor(
         uint256 entranceFee,
@@ -41,24 +55,39 @@ contract Raffle is VRFConsumerBaseV2Plus {
         i_subscriptionId = subscriptionId;
         i_callbackGasLimit = callbackGasLimit;
         s_lastTimeStamp = block.timestamp;
+        s_raffleState = RaffleState.OPEN;
     }
 
     // external 比 public 更便宜
     function enterRaffle() external payable {
         // require(msg.value >= i_entranceFee, "Not enough ETH sent");
         if (msg.value < i_entranceFee) revert Raffle_NotEnoughEthSent(); // 自定义Error类型，比require更便宜
+        if (s_raffleState != RaffleState.OPEN) revert Raffle_RaffleNotOpen();
         s_players.push(payable(msg.sender));
         emit EnteredRaffle(msg.sender);
     }
 
-    // 1. Get a random number
-    // 2. Use the random number to pick a player
-    // 3. Automatically called
-    function pickWinner() external {
-        // check to see if enough time has passed
-        if (block.timestamp - s_lastTimeStamp < i_interval) revert();
+    function checkUpkeep(
+        bytes memory /* checkData */
+    ) public view returns (bool upkeepNeeded, bytes memory /* performData */) {
+        bool timeHasPassed = (block.timestamp - s_lastTimeStamp) >= i_interval;
+        bool isOpen = s_raffleState == RaffleState.OPEN;
+        bool hasPlayers = s_players.length > 0;
+        bool hasBalance = address(this).balance > 0;
+        upkeepNeeded = (timeHasPassed && isOpen && hasPlayers && hasBalance);
+        return (upkeepNeeded, "");
+    }
 
-        uint256 requestId = s_vrfCoordinator.requestRandomWords(
+    function performUpkeep(bytes calldata /* performData */) external {
+        (bool upkeepNeeded, ) = checkUpkeep("");
+        if (!upkeepNeeded)
+            revert Raffle_UpkeepNotNeeded(
+                address(this).balance,
+                s_players.length,
+                uint256(s_raffleState)
+            );
+        s_raffleState = RaffleState.CALCULATING;
+        s_vrfCoordinator.requestRandomWords(
             VRFV2PlusClient.RandomWordsRequest({
                 keyHash: i_keyHash, // relate to gas price
                 subId: i_subscriptionId,
@@ -74,10 +103,22 @@ contract Raffle is VRFConsumerBaseV2Plus {
     }
 
     function fulfillRandomWords(
-        uint256 requestId,
+        uint256 /* requestId */,
         uint256[] calldata randomWords
     ) internal override {
-        // 暂时空实现
+        uint256 indexOfWinner = randomWords[0] % s_players.length;
+        address payable winner = s_players[indexOfWinner];
+        s_recentWinner = winner;
+        s_raffleState = RaffleState.OPEN;
+        // 在Solidity中，数组没有类似Python的.clear()方法
+        // 因为Solidity是在EVM上运行，没有自动垃圾回收机制
+        // 这里通过创建新的空数组来重置s_players，旧数组会被标记为未使用并释放存储空间
+        s_players = new address payable[](0);
+        s_lastTimeStamp = block.timestamp;
+        emit WinnerPicked(winner);
+
+        (bool success, ) = winner.call{value: address(this).balance}("");
+        if (!success) revert Raffle_TransferFailed();
     }
 
     /** Getter Function */
