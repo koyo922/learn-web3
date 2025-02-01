fd58b01b7f1104ad84bfddf5656345aa8bafa876
koyo922 <koyo922@qq.com>
2025-02-01 17:47:59 +0800

    ADD: testAllowance

--- updraft_course/foundry_erc20/test/OurTokenTest.t.sol
+++ updraft_course/foundry_erc20/test/OurTokenTest.t.sol
@@ -18,7 +18,7 @@ contract OurTokenTest is Test {
         console.log("Before deployment:");
         console.log("- test contract (this):", address(this));
         console.log("- msg.sender:", msg.sender);
-        
+
         deployer = new DeployOurToken();
         ourToken = deployer.run();
 
@@ -30,12 +30,24 @@ contract OurTokenTest is Test {
         console.log("- msg.sender balance:", ourToken.balanceOf(msg.sender));
         console.log("- this balance:", ourToken.balanceOf(address(this)));
         console.log("- deployer balance:", ourToken.balanceOf(address(deployer)));
-        
+
         vm.prank(msg.sender);
         ourToken.transfer(bob, STARTING_BALANCE);
     }
 
     function testBobBalance() public view {
         assertEq(ourToken.balanceOf(bob), STARTING_BALANCE);
+    }
+
+    function testAllowance() public {
+        uint256 initialAllowance = 1000;
+        vm.prank(bob);
+        ourToken.approve(alice, initialAllowance);
+
+        vm.prank(alice);
+        ourToken.transferFrom(bob, alice, initialAllowance);
+
+        assertEq(ourToken.balanceOf(alice), initialAllowance);
+        assertEq(ourToken.balanceOf(bob), STARTING_BALANCE - initialAllowance);
     }
 }
