e91326fbf4383178fac9e8987a0e486921455abe
koyo922 <koyo922@qq.com>
2025-02-02 16:36:12 +0800

    ADD: test convert svg to image uri

---
+++ updraft_course/foundry_nft/script/DeployMoodNft.s.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import {Script} from "forge-std/Script.sol";
+import {MoodNft} from "../src/MoodNft.sol";
+import {Base64} from "@openzeppelin/contracts/utils/Base64.sol";
+contract DeployMoodNft is Script {
+    function run() external {}
+
+    function svgToImageUri(string memory svg) public pure returns (string memory) {
+        return string.concat("data:image/svg+xml;base64,", Base64.encode(abi.encodePacked(svg)));
+    }
+}
---
+++ updraft_course/foundry_nft/test/DeployMoodNft.t.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import {Test} from "forge-std/Test.sol";
+import {DeployMoodNft} from "../script/DeployMoodNft.s.sol";
+
+contract DeployMoodNftTest is Test {
+    DeployMoodNft deployer;
+
+    function setUp() public {
+        deployer = new DeployMoodNft();
+    }
+
+    function testConvertSvgToImageUri() public view {
+        string memory svg = vm.readFile("./img/example.svg");
+        string memory actualUri = deployer.svgToImageUri(svg);
+        string
+            memory expectedUri = "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHhtbG5zOnhsaW5rPSJodHRwOi8vd3d3LnczLm9yZy8xOTk5L3hsaW5rIiB3aWR0aD0iNTAwIiBoZWlnaHQ9IjUwMCI+Cjx0ZXh0IHg9IjAiIHk9IjE1IiBmaWxsPSJibGFjayI+SGkhIFlvdXIgYnJvd3NlciBkZWNvZGVkIHRoaXM8L3RleHQ+Cjwvc3ZnPg==";
+        assertEq(actualUri, expectedUri, "URI should be equal");
+    }
+}
